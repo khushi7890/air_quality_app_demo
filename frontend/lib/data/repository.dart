6920b62671de5b982fe85fdf121e1fc5d64e3e51
Khushi Shah <email4khushiushah@gmail.com>
2025-11-07 13:12:06 -0700

    working on todoItem json parse

--- frontend/lib/data/repository.dart
+++ frontend/lib/data/repository.dart
@@ -6,17 +6,18 @@ import '../models/sensor_data.dart';
 
 class Repository {
   final ServiceFastAPI fastAPI;
-  final ServiceFirestore? firestore;
+  final ServiceFirestore firestore;
 
   Repository({required this.fastAPI, required this.firestore});
 
-  Future<List<TodoItem>> fetchTodos() async{
+  Future<List<TodoItem>> fetchTodos() async {
     // send to the fastAPI service to fetch todos
     return await fastAPI.fetchTodos();
   }
+
   Future<List<SensorModel>> fetchSensors() => fastAPI.getSensors();
 
-  // Future<void> addDemoSensorToFirestore() async {
-  //   await firestore.addDemoSensor();
-  // }
+  Future<void> addDemoSensorToFirestore() async {
+    await firestore.addDemoSensor();
+  }
 }
--- frontend/lib/screens/home_screen.dart
+++ frontend/lib/screens/home_screen.dart
@@ -1,30 +1,26 @@
 import 'package:flutter/material.dart';
-import '../data/service_fastapi.dart';
 import 'package:air_quality_app_demo/models//aqi_wheel.dart'; // adjust the import
 
 class HomeScreen extends StatelessWidget {
-
-  // dummy sensorData object for AQI display
+  // dummy sensor data to help build AQI wheel
+  // should get rid of soon
   final Map<String, dynamic> sensorData = {
     'timestamp': '2025-11-04T21:10:54.191488+00:00',
     'deviceId': 'PA100025',
-    'AQI': 150.0,
+    'AQI': 212.0,
     'AQI_dominant_pollutant': 'pm2.5'
   };
 
-  // displays our AQI Wheel widget
+  // Basic alignment and put in AQUI wheel
   @override
   Widget build(BuildContext context) {
-    //get dummy data
     final aqi = sensorData['AQI'];
     return Scaffold(
-      // title and basic alignment
       appBar: AppBar(title: const Text('Air Quality Sensor')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // feed into constructor - AQI wheel is a separate dart object
             AQIWheel(aqi: aqi),
             const SizedBox(height: 20),
           ],
